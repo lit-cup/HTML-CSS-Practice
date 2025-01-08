7ba5593c7b9dd80bac068a1c8aba9fd2582e336d
lit-cup <60121433+lit-cup@users.noreply.github.com>
2025-01-08 22:53:04 +0800

    Protfolio Add most of css js

---
+++ Practice5 portfolio website/js/language-select.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const languageSelect = document.querySelector('.language-select');
+    const selected = document.querySelector('.select-selected');
+    const items = document.querySelector('.select-items');
+    
+    selected.addEventListener('click', () => {
+        languageSelect.classList.toggle('open');
+    });
+    
+    items.addEventListener('click', (e) => {
+        if (e.target.closest('div')) {
+          const selectedItem = e.target.closest('div');
+          const imgSrc = selectedItem.querySelector('img').src;
+          const lang = selectedItem.getAttribute('data-lang');
+    
+          // 更新選中的內容
+          selected.innerHTML = `<img src="${imgSrc}" alt="icon">`;
+    
+          // 模擬語言切換邏輯
+          //alert(`切換到語言：${text}`);
+          languageSelect.classList.remove('open');
+        }
+    });
+    
+});
