0cc2fd126e572f5563d3a2034c1f2677b08e169c
lit-cup <60121433+lit-cup@users.noreply.github.com>
2025-02-04 17:00:14 +0800

    v7.3.0

--- Practice5 portfolio website/js/language-select.js
+++ Practice5 portfolio website/js/language-select.js
@@ -2,7 +2,37 @@ document.addEventListener('DOMContentLoaded', () => {
     const languageSelect = document.querySelector('.language-select');
     const selected = document.querySelector('.select-selected');
     const items = document.querySelector('.select-items');
+    const menuItems = document.querySelectorAll('nav .menu li a');
     
+    
+    //menuheightlight
+    menuItems.forEach(item => {
+        item.addEventListener('click', function() {
+            menuItems.forEach(i => i.classList.remove('active'));
+            this.classList.add('active');
+        });
+    });
+
+    window.addEventListener('scroll', function() {
+        let current = '';
+        const sections = document.querySelectorAll('section');
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+            if (window.scrollY >= sectionTop - sectionHeight / 3) {
+                current = section.getAttribute('id');
+            }
+        });
+
+        menuItems.forEach(item => {
+            item.classList.remove('active');
+            if (item.getAttribute('href').substring(1) === current) {
+                item.classList.add('active');
+            }
+        });
+    });
+    
+    //translate language
     selected.addEventListener('click', () => {
         languageSelect.classList.toggle('open');
     });
@@ -24,3 +54,4 @@ document.addEventListener('DOMContentLoaded', () => {
 
     
 });
+
