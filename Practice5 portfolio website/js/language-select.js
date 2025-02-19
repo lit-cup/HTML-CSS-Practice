7f8ad1f706460157dfc4897741e7d9c23ddf1ad8
lit-cup <60121433+lit-cup@users.noreply.github.com>
2025-02-19 17:02:58 +0800

    fix layout of @media(max-width: 48em)

--- Practice5 portfolio website/js/language-select.js
+++ Practice5 portfolio website/js/language-select.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const languageSelect = document.querySelector('.language-select');
+    const selectedOption = document.querySelector(".selected-option");
+    const dropdownItems = document.querySelectorAll(".menu-dropdown li")
     const selected = document.querySelector('.select-selected');
     const items = document.querySelector('.select-items');
     const menuItems = document.querySelectorAll('nav .menu li a');
@@ -15,12 +17,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
     window.addEventListener('scroll', function() {
         let current = '';
+        let selectoptionvalue = '';
         const sections = document.querySelectorAll('section');
         sections.forEach(section => {
             const sectionTop = section.offsetTop;
             const sectionHeight = section.clientHeight;
             if (window.scrollY >= sectionTop - sectionHeight / 3) {
                 current = section.getAttribute('id');
+                selectoptionvalue = section.getAttribute("data-value");
             }
         });
 
@@ -28,6 +32,7 @@ document.addEventListener('DOMContentLoaded', () => {
             item.classList.remove('active');
             if (item.getAttribute('href').substring(1) === current) {
                 item.classList.add('active');
+                selectedOption.textContent = selectoptionvalue;
             }
         });
     });
@@ -52,6 +57,13 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
+    // 監聽選單項目點擊事件
+    dropdownItems.forEach(item => {
+    item.addEventListener("click", () => {
+        const newValue = item.getAttribute("data-value"); // 取得點擊的值
+        selectedOption.textContent = newValue; // 更新固定顯示的選項
+    });
+});
     
 });
 
