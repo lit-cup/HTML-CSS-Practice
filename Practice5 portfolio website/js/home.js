8cdfacda670198e539190f476558a1235f6a4deb
lit-cup <60121433+lit-cup@users.noreply.github.com>
2025-02-24 15:41:09 +0800

    1. fix RWD Designed 2. Change js name to home 3. Chagne js for click menu

--- Practice5 portfolio website/js/home.js
+++ Practice5 portfolio website/js/home.js
@@ -1,30 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
     const languageSelect = document.querySelector('.language-select');
-    const selectedOption = document.querySelector(".selected-option");
+    const menuSelect = document.querySelector('.nav-links');
+    const selectedOption = document.querySelector(".about-me-selected-option");
     const dropdownItems = document.querySelectorAll(".menu-dropdown li")
-    const selected = document.querySelector('.select-selected');
-    const items = document.querySelector('.select-items');
+    const selected = document.querySelector('.language-option');
+    const languageOptionItems = document.querySelector('.languageOption-items');
     const menuItems = document.querySelectorAll('nav .menu li a');
     
     
-    //menuheightlight
+    //導覽列底線
     menuItems.forEach(item => {
         item.addEventListener('click', function() {
             menuItems.forEach(i => i.classList.remove('active'));
             this.classList.add('active');
         });
     });
-
+    //滑動螢幕的動作 1. 給section上底線 active 2. 中螢幕時替換導覽列文字
     window.addEventListener('scroll', function() {
         let current = '';
-        let selectoptionvalue = '';
+        let selectedOptionValue = '';
         const sections = document.querySelectorAll('section');
         sections.forEach(section => {
             const sectionTop = section.offsetTop;
             const sectionHeight = section.clientHeight;
-            if (window.scrollY >= sectionTop - sectionHeight / 3) {
+            if (window.scrollY >= sectionTop - sectionHeight / 4) {
                 current = section.getAttribute('id');
-                selectoptionvalue = section.getAttribute("data-value");
+                selectedOptionValue = section.getAttribute("data-value");
             }
         });
 
@@ -32,17 +33,23 @@ document.addEventListener('DOMContentLoaded', () => {
             item.classList.remove('active');
             if (item.getAttribute('href').substring(1) === current) {
                 item.classList.add('active');
-                selectedOption.textContent = selectoptionvalue;
+                selectedOption.textContent = selectedOptionValue;
             }
         });
     });
+
+    selectedOption.addEventListener('click',()=>{
+        menuSelect.classList.toggle('active');
+    })
+
+
     
     //translate language
     selected.addEventListener('click', () => {
         languageSelect.classList.toggle('open');
     });
     
-    items.addEventListener('click', (e) => {
+    languageOptionItems.addEventListener('click', (e) => {
         if (e.target.closest('div')) {
           const selectedItem = e.target.closest('div');
           const imgSrc = selectedItem.querySelector('img').src;
@@ -62,8 +69,8 @@ document.addEventListener('DOMContentLoaded', () => {
     item.addEventListener("click", () => {
         const newValue = item.getAttribute("data-value"); // 取得點擊的值
         selectedOption.textContent = newValue; // 更新固定顯示的選項
+        menuSelect.classList.remove('active');
     });
 });
     
 });
-
