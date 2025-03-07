76c07911ca922c1694127d0268d7aa9112287259
lit-cup <60121433+lit-cup@users.noreply.github.com>
2025-03-07 16:23:21 +0800

    feat: Create a simple Guess the Number project
    - document.querySelectore('class') pratice
    - Handle Guess Number bigger/small/empty check event

--- Practice6 Javascript Enhances/Project1_GuessTheNumber/index.js
+++ Practice6 Javascript Enhances/Project1_GuessTheNumber/index.js
@@ -2,8 +2,48 @@
 
 //DONE: 
 //Guess the number Game UI
+let score = 20;
+const scoreValue = document.querySelector('.score-value');
+const message = document.querySelector('.label-message');
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-
+//check btn click event
+document.querySelector('.btn-check').addEventListener('click', function(){
+    const guess = Number(document.querySelector('.guess').value);
+    //empty check
+    if(!guess){
+        message.textContent = '⛔ No Number!';
+    }
+    //When guess is correct
+    if(guess === secretNumber){
+        message.textContent = '🎉 Correct Number!';
+        document.querySelector('body').style.backgroundColor = '#fffb00';
+        document.querySelector('.number').style.width = '30rem';
+        document.querySelector('.number').textContent = secretNumber;
+    }
+    //When guess is bigger than secret number
+    else if(guess > secretNumber){
+        if(score > 1){
+            message.textContent = '📈 Too High!';
+            score--;
+            scoreValue.textContent = score;
+        }else{          
+            message.textContent = '💥 You lost the game!';
+            scoreValue.textContent = 0;
+        }
+    }
+    //When guess is smaller than secret number
+    else if(guess < secretNumber){
+        if(score > 1){
+            message.textContent = '📉 Too Low!';
+            score--;
+            scoreValue.textContent = score;
+        }else{
+            message.textContent = '💥 You lost the game!';
+            scoreValue.textContent = 0;
+        }
+    }
+});
 
 //TODO:
 // 1. Practice DOM Manipulation
